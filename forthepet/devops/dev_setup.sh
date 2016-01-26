4f3ffd4a2d743eb3ad7053d088bd06b320912c5c
Glen Bray <glen.bray@gmail.com>
2016-01-26 20:50:29 +1100

    fix errors that occur during `vagrant up`

--- forthepet/devops/dev_setup.sh
+++ forthepet/devops/dev_setup.sh
@@ -4,8 +4,13 @@ sudo ufw allow 3000
 sudo chown -R vagrant:vagrant /usr/local/rbenv/
 ssh-keyscan github.com >> /home/vagrant/.ssh/known_hosts
 
-sudo apt-get install sqlite3 libsqlite3-dev postgresql-client-9.3
+sudo apt-get -y install sqlite3 libsqlite3-dev postgresql-client-9.3
 gem install mailcatcher
+
+
+sudo curl -o /usr/local/bin/forego -O https://godist.herokuapp.com/projects/ddollar/forego/releases/current/linux-amd64/forego
+sudo chown vagrant:vagrant /usr/local/bin/forego
+sudo chmod a+x /usr/local/bin/forego
 
 gem install foreman
 cd /forthepet && bundle install
@@ -22,5 +27,6 @@ cd /forthepet && rake db:create
 
 echo 'cd /forthepet' >> /home/vagrant/.bashrc
 echo 'export COVERAGE=true' >> /home/vagrant/.bashrc
+echo 'alias b="bundle exec"' >> /home/vagrant/.bashrc
 
 # curl http://j.mp/spf13-vim3 -L -o - | sh
