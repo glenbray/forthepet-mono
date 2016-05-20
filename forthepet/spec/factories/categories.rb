72f7859903afa5b54fc04aeed490810477aa8c1c
Glen Bray <glen.bray@gmail.com>
2016-05-20 20:39:00 +1000

    fix broken specs

--- forthepet/spec/factories/categories.rb
+++ forthepet/spec/factories/categories.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  factory :category do
+    name 'default'
+  end
+
   factory :all, class: Category do
     name 'All'
   end
