6bc72ab5acd880fead55c9869287e3eea5fb0130
Glen Bray <glen.bray@gmail.com>
2016-08-24 18:43:33 +1000

    update dependencies

--- forthepet/config/deploy.rb
+++ forthepet/config/deploy.rb
@@ -6,7 +6,7 @@ set :deploy_to, '/var/www/forthepet'
 set :repo_url, 'git@github.com:glenbray/forthepet-meta.git'
 set :repo_tree, "forthepet"
 
-set :rbenv_ruby, "#{File.read('.ruby-version').chomp}"
+set :rbenv_ruby, File.read('.ruby-version').chomp
 set :rbenv_type, :system
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
