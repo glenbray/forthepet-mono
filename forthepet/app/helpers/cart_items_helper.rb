2db737f7a840bd0af337b3ccdc52dedaaa93e572
Duc Giang <giangnguyen1089@gmail.com>
2017-01-12 16:42:04 +0700

    minor fixes and improvements

--- forthepet/app/helpers/cart_items_helper.rb
+++ forthepet/app/helpers/cart_items_helper.rb
@@ -15,5 +15,20 @@ module CartItemsHelper
       ['Delivery Every 6 Months', 'six_months']
     ]
   end
+
+  def short_delivery_frequency_options
+    [
+      ['1 Week', 'one_week'],
+      ['2 Weeks', 'two_weeks'],
+      ['3 Weeks', 'three_weeks'],
+      ['4 Weeks', 'four_weeks'],
+      ['5 Weeks', 'five_weeks'],
+      ['6 Weeks', 'six_weeks'],
+      ['7 Weeks', 'seven_weeks'],
+      ['8 Weeks', 'eight_weeks'],
+      ['3 Months', 'three_months'],
+      ['6 Months', 'six_months']
+    ]
+  end
 end
 
