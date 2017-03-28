00c6ae4fe5c7695f896bc99208920ccdf0dd6eba
Glen Bray <glen.bray@gmail.com>
2017-03-29 01:52:37 +1100

    fix nil bug

--- forthepet/app/helpers/shopping/checkout_helper.rb
+++ forthepet/app/helpers/shopping/checkout_helper.rb
@@ -13,9 +13,11 @@ module Shopping::CheckoutHelper
   end
 
   def expire_month
-    [['01 January', 1], ['02 Feburary', 2], ['03 March', 3], ['04 April', 4],
-     ['05 May', 5], ['06 June', 6], ['07 July', 7], ['08 August', 8], ['09 September', 9],
-     ['10 October', 10], ['11 November', 11], ['12 December', 12]]
+    [
+      ['01 January', 1], ['02 Feburary', 2], ['03 March', 3], ['04 April', 4],
+      ['05 May', 5], ['06 June', 6], ['07 July', 7], ['08 August', 8], ['09 September', 9],
+      ['10 October', 10], ['11 November', 11], ['12 December', 12]
+    ]
   end
 
   def credit_card_no
